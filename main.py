b151d6764f1aee617803536e601d54d7fad30d06
bychkov.e <killup.killup@yandex.ru>
2022-12-22 17:13:57 +0500

    Добавлен файл setting с настройками + создан макет основного игрового цикла.

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 # Quarryman GAME
+import pygame
+import os
+import sys
+
+from setting import *
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Копатель')
+    size = WIDTH, HEIGHT
+    screen = pygame.display.set_mode(size)
+    running = True
+    clock = pygame.time.Clock()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+    pygame.quit()
+
