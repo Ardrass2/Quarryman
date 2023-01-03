5a8de332f09ae5c13e0743d0d9ac8b6e655ffc4c
ardrass <Ardrass@mail.ru>
2023-01-04 01:01:14 +0500

    Добавлен класс Miner отвечающий за действия персонажа в шахте. Добавлена возможнорсть(криво) разушать блоки. Реализован переход в шахту(созданы функции в main.py, которые содержат разные игровые циклы)

--- character.py
+++ character.py
@@ -4,6 +4,68 @@ from function import *
 from setting import *
 
 
+class Miner(pygame.sprite.Sprite):
+    def __init__(self, all_sprites, sheet, columns, rows):
+        super().__init__(all_sprites)
+        self.action = {"stay": 2,
+                       "run": 1}
+        self.sheet = sheet
+        self.columns, self.rows = columns, rows
+        self.frames = []
+        self.now_action = ""
+        self.time = 0
+        self.cut_sheet("stay")
+        self.cur_frame = 0
+        self.image = pygame.transform.scale(self.frames[self.cur_frame], (width * 0.1, height * 0.2))
+        self.rect.center = width // 2, height // 2
+        self.some = False
+
+    def cut_sheet(self, action):
+        self.frames = []
+        self.rect = pygame.Rect(0, 0, self.sheet.get_width() // self.columns,
+                                self.sheet.get_height() // self.rows)
+        for i in range(self.columns):
+            frame_location = (self.rect.w * i, self.rect.h * self.action[action])
+            self.frames.append(self.sheet.subsurface(pygame.Rect(
+                frame_location, self.rect.size)))
+        self.now_action = action
+
+    def update(self, ground, key_down=0):
+        if len(pygame.sprite.spritecollide(self, ground, False)) == 0:
+            self.rect = self.rect.move(0, 1)
+        if self.time == 5:
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = pygame.transform.scale(self.frames[self.cur_frame], (width * 0.1, height * 0.2))
+            if self.some:
+                self.some = True
+                self.image = pygame.transform.flip(self.image, True, False)
+            self.time = 0
+        else:
+            self.time += 1
+        if key_down == pygame.K_s:
+            self.destroy_dirt(ground)
+            self.rect[1] = self.rect[1] + 124
+        if key_down == pygame.K_d:
+            self.rect[0] = self.rect[0] + 125
+            self.destroy_dirt(ground)
+            if not self.some:
+                self.some = True
+                self.image = pygame.transform.flip(self.image, True, False)
+        if key_down == pygame.K_a:
+            self.rect[0] = self.rect[0] - 125
+            if self.some:
+                self.some = False
+                self.image = pygame.transform.flip(self.image, True, False)
+            self.destroy_dirt(ground)
+
+    def destroy_dirt(self, sprites):
+        if pygame.sprite.spritecollide(self, sprites, False):
+            pygame.sprite.spritecollideany(self, sprites).kill()
+
+    def collide_with(self, sprites):
+        return pygame.sprite.spritecollide(self, sprites, False)
+
+
 class Digger(pygame.sprite.Sprite):
     def __init__(self, all_sprites, sheet, columns, rows):
         super().__init__(all_sprites)
@@ -44,9 +106,8 @@ class Digger(pygame.sprite.Sprite):
         else:
             self.time += 1
         if type(args[0]) != int and type(args[1]) != int:
-            if not pygame.sprite.collide_mask(self, args[0]):
+            if not self.check_collide(args[0]):
                 self.rect = self.rect.move(0, 1)
-                x, y = self.rect[0], self.rect[1]
         if not args[1]:
             if self.now_action != "stay":
                 own_rect = self.rect[0], self.rect[1]
@@ -71,13 +132,13 @@ class Digger(pygame.sprite.Sprite):
                 self.some = False
                 self.image = pygame.transform.flip(self.image, True, False)
             self.left_or_right = -1
-        if not self.rect[0] >= width * 0.87 and not self.rect[0] <= -(width * 0.06):
+        if not self.rect[0] >= width * 0.9 and not self.rect[0] <= -(width * 0.01):
             self.rect = self.rect.move(self.left_or_right * self.speed, 0)
         else:
             if self.rect[0] > width * 0.5:
                 self.rect[0] = width * 0.87
             else:
-                self.rect[0] = -width * 0.06
+                self.rect[0] = 0
 
     def check_collide(self, sprite):
         if pygame.sprite.collide_mask(self, sprite):
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pygame_gui
 
 from character import *
 from first_location import *
-from function import *
 from mining_location import *
 from music_player import *
 from setting import *
@@ -12,14 +11,76 @@ from settings_window import Settings_Window
 from start_window import Start_Window
 
 
-def main():
-    pygame.init()
-    pygame.display.set_caption('Копатель')
-    size = width, height
-    music = Music()
-    all_sprites = pygame.sprite.Group()
-    screen = pygame.display.set_mode(size)
-    clock = pygame.time.Clock()
+def start_mine():
+    digger = Miner(all_sprites, load_image("texture/miner.png"), 10, 5)
+    bg = pygame.transform.scale(load_image("texture/cave_mining.jpg"), (width, height))
+    screen.blit(bg, (0, 0))
+    while True:
+        time_delta = clock.tick(FPS) / 1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN:
+                flag = True
+                digger.update(tiles_group, event.key)
+        screen.blit(bg, (0, 0))
+        tiles_group.update()
+        tiles_group.draw(screen)
+        all_sprites.update(tiles_group)
+        all_sprites.draw(screen)
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def upper_world_cycle():
+    grass = Grass(all_sprites)
+    mine = Mine(all_sprites)
+    shop = Shop(all_sprites)
+    digger = Digger(all_sprites, load_image("texture/miner.png"), 10, 5)
+    bg = pygame.transform.scale(load_image("texture/sky.png"), (width, height * 2 // 3))
+    press_e = None
+    manager = pygame_gui.UIManager((width, height))
+    manager.get_theme().load_theme('game_theme.json')
+
+    while True:
+        time_delta = clock.tick(FPS) / 1000.0
+        if (digger.check_collide(mine) or digger.check_collide(shop)) and press_e is None:
+            press_e = pygame_gui.elements.UILabel(manager=manager, text="Нажмите E, чтобы войти",
+                                                  relative_rect=pygame.Rect((width * 0.5, 0),
+                                                                            (width * 0.5, height * 0.1)),
+                                                  object_id=pygame_gui.core.ObjectID(class_id='@game_text',
+                                                                                     object_id='#help_text'))
+        elif not (digger.check_collide(mine) or digger.check_collide(shop)) and press_e is not None:
+            press_e = None
+            manager.clear_and_reset()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_e and digger.check_collide(mine):
+                    for elem in all_sprites:
+                        elem.kill()
+                    manager.clear_and_reset()
+                    generate_mine(all_sprites, tiles_group)
+                    start_mine()
+                if event.key == pygame.K_e and digger.check_collide(shop):
+                    print("Shop")
+                flag = True
+                digger.update(event.key, flag)
+            if event.type == pygame.KEYUP:
+                flag = False
+                digger.update(event.key, flag)
+        screen.blit(bg, (0, 0))
+        all_sprites.update(grass, mine)
+        all_sprites.draw(screen)
+        manager.update(time_delta=time_delta)
+        manager.draw_ui(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def first_step():
     start = Start_Window(clock)
     if start.next_window == "setting":
         while start.next_window == "setting":
@@ -27,51 +88,17 @@ def main():
             if setting_window.back == "back":
                 start.next_window = start.main_cycle(clock)
     if start.next_window == "game":
-        grass = Grass(all_sprites)
-        mine = Mine(all_sprites)
-        shop = Shop(all_sprites)
-        digger = Digger(all_sprites, load_image("texture/miner.png"), 10, 5)
-        bg = pygame.transform.scale(load_image("texture/sky.png"), (width, height * 2 // 3))
-        screen.blit(bg, (0, 0))
-        press_e = None
-        manager = pygame_gui.UIManager((width, height))
-        manager.get_theme().load_theme('game_theme.json')
-
-        while True:
-            time_delta = clock.tick(FPS) / 1000.0
-            flag = True
-            if (digger.check_collide(mine) or digger.check_collide(shop)) and press_e is None:
-                press_e = pygame_gui.elements.UILabel(manager=manager, text="Нажмите E, чтобы войти",
-                                                      relative_rect=pygame.Rect((width * 0.5, 0),
-                                                                                (width * 0.5, height * 0.1)),
-                                                      object_id=pygame_gui.core.ObjectID(class_id='@game_text',
-                                                                                         object_id='#help_text'))
-            elif not (digger.check_collide(mine) or digger.check_collide(shop)) and press_e is not None:
-                press_e = None
-                manager.clear_and_reset()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    terminate()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_e and digger.check_collide(mine):
-                        tiles_group = pygame.sprite.Group()
-                        generate_mine(all_sprites, tiles_group)
-                    if event.key == pygame.K_e and digger.check_collide(shop):
-                        print("Shop")
-                    flag = True
-                    digger.update(event.key, flag)
-                if event.type == pygame.KEYUP:
-                    flag = False
-                    digger.update(event.key, flag)
-            screen.blit(bg, (0, 0))
-            all_sprites.update(grass, mine)
-            all_sprites.draw(screen)
-            manager.update(time_delta=time_delta)
-            manager.draw_ui(screen)
-            pygame.display.flip()
-            clock.tick(FPS)
+        upper_world_cycle()
 
 
 if __name__ == '__main__':
-    main()
+    pygame.init()
+    pygame.display.set_caption('Копатель')
+    size = width, height
+    music = Music()
+    all_sprites = pygame.sprite.Group()
+    tiles_group = pygame.sprite.Group()
+    screen = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
+    first_step()
     sys.excepthook = except_hook
